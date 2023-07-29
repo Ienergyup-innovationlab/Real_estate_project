5f499062b107982aea45e2c5e717b30d7b4ca8bd
Michael ogozi <michealogozi@gmail.com>
2023-07-29 14:38:39 +0300

    rearrnging components

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-import ReusableButton from "./ReusableButton";
+import ReusableButton from "./components/ReusableButton";
 import logo1 from "./images/logo1.png";
 import { ArrowDropDown } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { yellow } from "@mui/material/colors";
-import DropdownMenu from "./DropdownMenu";
+import DropdownMenu from "./components/DropdownMenu";
 
 function Header() {
   const [openDropdown, setOpenDropdown] = useState(false);
   return (
     <>
       <header className="header  flex justify-between max-w-full h-14 md:h-20  bg-customBlue items-center sticky z-10 top-0">
-        <div className=" header_left pl-9 ">
+        <div className=" header_left pl-9 cursor-pointer">
           <img
             className="w-10 md:w-16 h14 md:h-20 "
             src={logo1}
             alt=" company logo"
           />
         </div>
-        <div className="header_middle hidden md:flex font-bold text-white">
+        <div className="header_middle hidden md:flex font-bold text-white cursor-pointer">
           <ul className="flex justify-around">
             <li className=" px-4">Home </li>
             <li className=" px-4">About </li>
