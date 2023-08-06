879fffa34c16fefeab39f29faceca1cd4705e8bc
Michael ogozi <michealogozi@gmail.com>
2023-08-06 20:35:50 +0300

    landing page mobile responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
   const [openDropdown, setOpenDropdown] = useState(false);
   return (
     <>
-      <header className="header  flex justify-between max-w-full h-14 md:h-20  bg-customBlue items-center sticky z-10 top-0">
+      <header className="header md:h-20 flex justify-between max-w-full h-14  bg-customBlue items-center sticky z-10 top-0">
         <div className=" header_left pl-9 cursor-pointer">
           <img
             className="w-10 md:w-16 h14 md:h-20 "
