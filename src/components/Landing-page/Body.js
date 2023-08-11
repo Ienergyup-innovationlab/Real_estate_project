f39255c6dee030dd0041b7c26dfc389cfa6004e2
Michael ogozi <michealogozi@gmail.com>
2023-08-11 13:12:51 +0300

    linked agent signup and house owner signup

--- src/components/Landing-page/Body.js
+++ src/components/Landing-page/Body.js
@@ -1,62 +1,72 @@
 import React from "react";
-import landingimage from "../images/landingpageimage.jpg";
-import extra1 from "../images/extra1.jpg";
-import extra2 from "../images/extra2.jpg";
-import extra3 from "../images/extra3.jpg";
-import newsletterimage from "../images/newsletterimage.png";
-import productCategoryImage from "../images/productCategoriesImage.jpg";
-import ServicesCategoryImage from "../images/ServicesCategoriesImage.png";
+import landingimage from "../Landing-page/images/landingpageimage.jpg";
+import extra1 from "../Landing-page/images/extra1.jpg";
+import extra2 from "../Landing-page/images/extra2.jpg";
+import extra3 from "../Landing-page/images/extra3.jpg";
+import newsletterimage from "../Landing-page/images/newsletterimage.png";
+import productCategoryImage from "../Landing-page/images/productCategoriesImage.jpg";
+import ServicesCategoryImage from "../Landing-page/images/ServicesCategoriesImage.png";
 import { ArrowRight, ArrowRightAlt, WhatsApp } from "@mui/icons-material";
-import ReusableButton from "./ReusableButton";
-import TransparentReusableBtn from "./TransparentReusableBtn";
-import BuyerAgentSignup from "./BuyerAgentSignup";
+import ReusableButton from "../ReusableButton";
+import TransparentReusableBtn from "../TransparentReusableBtn";
+import BuyerAgentSignup from "../BuyerAgentSignup";
+import { PropertySearch } from "../home-screen/HomePage";
 // import TestimonialCard from "../TestimonialCard";
 
 function Body(btn_txt) {
   return (
     <div className=" container mx-auto">
-      <div className=" z-0 pt-3 md:pt-10 top-32 md:h-[40rem]">
-        <img src={landingimage} alt="" />
+      <div>
+        <img
+          src={landingimage}
+          alt=""
+          className=" z-0 mt-3 md:mt-10  md:h-[40rem] w-screen"
+        />
+        <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
+        <PropertySearch />
       </div>
-      <div className="flex-col  md:flex-row flex justify-evenly items-center mt-5 md:">
-        <div className="  md:h-[43rem] md:w-[33rem] ">
+      <div className="flex-col  md:flex-row flex justify-evenly items-center mt-10 md:mt-14">
+        <div className="  md:h-[43rem] md:w-[33rem] flex flex-col items-center justify-center">
           <img
             className="w-[21rem] h-[19rem] md:h-[30rem] md:w-[30rem] "
             src={productCategoryImage}
             alt=""
           />
-          <h4 className=" sm:mr-4 text-2xl font-bold text-customBlue">
-            Product Category
-          </h4>
-          <p className=" text-xs md:text-base text-start md:text-left">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam proin
-            quis auctor sit consectetur dolor nisi volutpat elit. Sed turpis
-            convallis velit pulvinar lectus nec habitant.
-          </p>
-          <h5 className=" font-semibold decoration-from-font pt-7">
-            view more categories
-            <ArrowRightAlt />
-          </h5>
+          <div>
+            <h4 className=" sm:mr-4 text-2xl font-bold text-customBlue text-start md:ml-4 ml-6">
+              Product Category
+            </h4>
+            <p className=" text-xs md:text-base text-start md:text-left md:px-4 px-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+              proin quis auctor sit consectetur dolor nisi volutpat elit. Sed
+              turpis convallis velit pulvinar lectus nec habitant.
+            </p>
+            <h5 className=" font-semibold decoration-from-font pt-7 text-end">
+              view more categories
+              <ArrowRightAlt />
+            </h5>
+          </div>
         </div>
-        <br />
-        <div className="md:h-[43rem] md:w-[33rem] ">
+        <div className="md:h-[43rem] md:w-[33rem] flex flex-col items-center justify-center">
           <img
             className="w-[21rem] h-[19rem]  md:h-[30rem] md:w-[30rem] "
             src={ServicesCategoryImage}
             alt=""
           />
-          <h4 className="text-2xl font-bold font-sans text-customBlue">
-            Services Category
-          </h4>
-          <p className=" text-xs md:text-base text-start md:text-left">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam proin
-            quis auctor sit consectetur dolor nisi volutpat elit. Sed turpis
-            convallis velit pulvinar lectus nec habitant.
-          </p>
-          <h5 className=" font-semibold decoration-from-font pt-7">
-            view more categories
-            <ArrowRightAlt />
-          </h5>
+          <div>
+            <h4 className="text-2xl font-bold font-sans text-customBlue text-start md:ml-4 ml-6">
+              Services Category
+            </h4>
+            <p className=" text-xs md:text-base text-start md:text-left md:px-4 px-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+              proin quis auctor sit consectetur dolor nisi volutpat elit. Sed
+              turpis convallis velit pulvinar lectus nec habitant.
+            </p>
+            <h5 className=" font-semibold decoration-from-font pt-7 text-end">
+              view more categories
+              <ArrowRightAlt />
+            </h5>
+          </div>
         </div>
       </div>
       <div className=" flex-col h-[39rem] md:h-[40rem] bg-customBlue flex md:flex-row items-center justify-evenly">
@@ -83,7 +93,6 @@ function Body(btn_txt) {
           view more <ArrowRight />
         </button>
       </div>
-
       <div className=" h-[43rem] w-auto md:bg-customBlue mb-20 flex flex-col justify-center items-center md:pb-20">
         <div className="text-center">
           <h2 className="font-bold text-customBlue md:text-amber-600 font-sans text-lg md:text-xl md:pb-10">
