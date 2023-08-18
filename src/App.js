0a83204316d322b2bc107218d2f198516d78431c
Michael ogozi <michealogozi@gmail.com>
2023-08-19 00:47:07 +0300

    Added mobile responsiveness of about page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import Login from "./components/Login";
 import HouseOwnerSignUp from "./components/HouseOwnerSignUp";
 import AboutPage from "./components/About-page/aboutPage";
+import Agents from "./components/Agents";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       {/*--path="*" helps display an error && navigate to "pageNotFound" if user navigates to a page that doesnt exist*/}
       <Route path="*" element={<PageNotFound />} />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/AgentSignup" element={<AgentSignUP />} />
+      <Route path="/Agents" element={<Agents />} />
       <Route path="/HouseOwnerSignup" element={<HouseOwnerSignUp />} />
       <Route path="/RealtorSignup" element={<HouseOwnerSignUp />} />
       <Route path="/CustomerSignup" element={<HouseOwnerSignUp />} />
