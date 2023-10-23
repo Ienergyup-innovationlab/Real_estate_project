ab5e1a49e10f96d75445807912e47322f56a4d52
shakiogunlesi <shaki_ogunlesi@outlook.com>
2023-10-23 10:40:34 +0100

    created property and services page

---
+++ src/components/serviceview/serviceview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Servicedetails from '../servicedetails';
+
+
+
+function propertyview () {
+    return (
+        <>
+        <Servicedetails />
+        </>
+    );
+}
+
+export default propertyview;
